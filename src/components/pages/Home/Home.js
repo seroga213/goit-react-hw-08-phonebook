74bf1d12713db6ddbcbeaafa794b5a86bbbf1a0e
Seroga Cherkassov <serogaseroga05@gmail.com>
2022-12-02 21:35:31 +0200

    done

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,13 +1,13 @@
 import s from '../Home/Home.module.css';
+import { Link } from 'react-router-dom';
 
 export const Home = () => {
   return (
     <>
       <h1 className={s.title}>Wellcome to Phonebook service</h1>
       <p className={s.subTitle}>
-        On this site you can save your phone book contacts. All contacts in one
-        place! You can open this site on any device, login and see your contacts
-        list.
+      please <Link to="/login">login</Link> or{' '}
+          <Link to="/registration">register</Link> to use
       </p>
     </>
   );
